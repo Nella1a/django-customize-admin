7edafe1779c8955d226e6cb38bb03eec3cf33502
MarthaN <>
2023-01-17 20:13:19 +0100

    Add filters to the list screen

--- core/admin.py
+++ core/admin.py
@@ -2,28 +2,39 @@ from django.contrib import admin
 
 # Register your models here.
 from core.models import Person, Course, Grade
-
+from django.db.models import Avg
+from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.http import urlencode
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "show_average")
 
     def show_average(self, obj):
-        from django.db.models import Avg
-        from django.utils.html import format_html
         result = Grade.objects.filter(person=obj).aggregate(Avg("grade"))
         return format_html("<b><i>{}</i></b>", result["grade__avg"])
 
     show_average.short_description = "Average Grade"
 
 
-
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "year", "view_students_link")
+    list_filter = ("year", )
+
+    def view_students_link(self,obj):
+        count = obj.person_set.count()
+        url = (
+            reverse("admin:core_person_changelist")
+            + "?"
+            + urlencode({"courses__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} Students</a>', url, count)
 
 
 @admin.register(Grade)
 class GradeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("person", "grade", "course")
+    list_filter = ("person__first_name", )
 
