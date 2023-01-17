e69ed220eadad1baaa722b43b5b2bd10368bfdb3
MarthaN <>
2023-01-17 17:28:04 +0100

    Modifying a change list

--- core/models.py
+++ core/models.py
@@ -9,12 +9,19 @@ class Person(models.Model):
     first_name = models.TextField()
     courses = models.ManyToManyField("Course", blank=True)
 
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
     class Meta:
         verbose_name_plural = "People"
+        ordering = ("last_name", "first_name")
 
 class Course(models.Model):
     name = models.TextField()
     year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}, {self.year}"
 
     class Meta:
         unique_together = ("name", "year", )
@@ -22,6 +29,10 @@ class Course(models.Model):
 class Grade(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     grade = models.PositiveSmallIntegerField(
-        validators = [MinValueValidator(0), MaxValueValidator(100)]
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.grade}, {self.person}, {self.course}"
+
